1bcadd9dab961c5bca4084935d450bc2d66b4e97
anogh <anoghkn@gmail.com>
2026-02-03 17:35:48 +0800

    AUTO: Update theme colors and logo  theme-setup.php, style.css, logo.png  2026-02-03 17:35:00

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -29,9 +29,10 @@ function warafy_enqueue_scripts() {
         theme: {
           extend: {
             colors: {
-              "primary": "#137fec",
+              "primary": "#dd1a1e",
+              "secondary": "#fcb711",
               "background-light": "#f6f7f8",
-              "background-dark": "#101922",
+              "background-dark": "#000000",
             },
             fontFamily: {
               "display": ["Inter", "sans-serif"]
