a2797ebce9ac3cc4dd19bab27c8b8b6e9bcc9537
anogh <anoghkn@gmail.com>
2026-02-15 15:12:39 +0800

    AUTO: Update logo and favicon from user request  header.php, assets  2026-02-15 15:20:00

--- header.php
+++ header.php
@@ -28,6 +28,7 @@ $is_facebook_webview = (
 ?>
 <html <?php language_attributes(); ?> class="light">
 <head>
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.jpg" type="image/jpeg" />
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://cdn.tailwindcss.com">
